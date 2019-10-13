2a983aafd133c638905b5a8b4aa7e02aac9076c3
Matthew Willard <MWillard85@gmail.com>
2019-10-13 12:36:19 -0600

    fixed issues

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function aboutMe() {
     <section id="aboutMePage" role="main">
     <h2>Matthew Willard</h2> 
     <p>Hi, I’m Matthew. I’m a web developer based in the Salt Lake City, Utah area. I love sports, movies, music, and all things tech. I’m currently 
-    trying to expand my knowledge of HTML, CSS, Javascript, Jquery, React, Express, Node.js, and PostgreSQL. I spent over five years of my life managing the cleanliness of 
+    trying to expand my knowledge of HTML, CSS, Javascript, Jquery, React, Express, Node.js, and PostgreSQL. I love coding because it allows me to 
+    express my creativity and it appeals to my desire to solve problems.  I spent over five years of my life managing the cleanliness of 
     a recreation center so I’m sort of a neat freak. I also love woodworking with dreams of making a rocking chair and canoe one day.</p>
   </section>
   `
@@ -25,7 +26,7 @@ function projects() {
   return `
   <section id="projectsPage" role="main">
   <h2>Training Tracker App</h2>    
-  <img id="training" src="images/trainingtracker.png" width="700" height="500" alt="preview of training tracker App"> 
+  <img id="training" src="images/trainingtracker.png" width="800" height="500" alt="preview of training tracker App"> 
   <p class="projectsAbout">An app for managers to track the trainings of their employees. Login required, create account or use Username: Demo -  Password: Password0!  Built with HTML, CSS, React, Node.js, PostgreSQL, and Express.js. 
   </p>
   <div>
@@ -43,7 +44,7 @@ function projects() {
     </section>
     <section id="projectsPage" role="main">
   <h2>Sports News App</h2>    
-  <img id="Sports" src="images/nflNews.png" width="700" height="500" alt="preview of sports App"> 
+  <img id="Sports" src="images/nflNews.png" width="800" height="500" alt="preview of sports App"> 
   <p class="projectsAbout">A sports news app that gives you all the latest NFL, NHL, and news from ESPN. Login required, create account or use Username: TestName -  Password: Password0!  Built with HTML, CSS, React, Node.js, PostgreSQL, and Express.js. 
   </p>
   <div>
@@ -61,7 +62,7 @@ function projects() {
     </section>
     <section id="projectsPage" role="main">
     <h2>Event Search App</h2>    
-    <img id="event" src="images/apiapp.png" width="700" height="500" alt="preview of Events App"> 
+    <img id="event" src="images/apiapp.png" width="800" height="500" alt="preview of Events App"> 
     <p class="projectsAbout">My Event Search app is a basic API app that shows you local sporting and musical events in a searched location.
       The user inputs their location and the most popular events are returned</p>
     <div>
@@ -76,7 +77,7 @@ function projects() {
   </section>
   <section id="projectsPage" role="main">
     <h2>Game of Thrones Quiz</h2>
-    <img id="thrones" src="images/quiz.png" width="700" height="500" alt="preview of Game of Thrones Quiz"> 
+    <img id="thrones" src="images/quiz.png" width="750" height="500" alt="preview of Game of Thrones Quiz"> 
     <p class="projectsAbout">My Game of Thrones quiz is a short ten question app.  It quizzes you on house sigils and words.
     After every question accurate feedback is received and at the end of the quiz the user receives an 
     out of 10 score.</p>
